b21a93ecc471a9076fde52017af5b7bcc31d1119
Naud14 <n.drost14@gmail.com>
2024-06-12 10:00:14 +0200

    Add input validation

--- auth.py
+++ auth.py
@@ -1,8 +1,9 @@
 import random
 from datetime import datetime
-
+import re
 # Define roles
 roles = ["super_admin", "system_admin", "consultant"]
+cities = ["Rotterdam", "Oldenzaal", "Budapest", "Kathmandu", "Moskou", "Lelystad", "Stockholm", "Ruinerwold", "Liverpool", "Penemunde"]
 
 
 # Add a user function
@@ -33,6 +34,18 @@ def verify_login(conn, username, password):
         print(e)
     return None
 
+
+# Add hardcoded super admin
+add_user(conn, "super_admin", "Admin_123?", "super_admin", "Super", "Admin")
+
+
+# Example login verification
+role = verify_login(conn, "super_admin", "Admin_123?")
+if role:
+    print(f"Login successful. Role: {role}")
+else:
+    print("Invalid username or password.")
+
 def create_unique_id():
     # get current year and convert to 2 digit string
     current_year = str(datetime.now().year)[-2:]
@@ -47,3 +60,26 @@ def create_unique_id():
     checksum = sum(first_nine) % 10
 
     return ''.join(map(str, first_nine)) + str(checksum)
+
+def check_first_name(name):
+    return name.isalpha()
+
+def validate_(age):
+    return age.isdigit() and 0 < int(age) <= 125
+
+def validate_gender(gender):
+    return gender.lower() in ['male', 'female']
+
+def validate_weight(weight):
+    return weight.replace('.', '', 1).isdigit() and 0 < int(weight) <= 700
+
+def validate_address(street, house_number, zip_code, city):
+    validate_zipcode = re.match(r'^\d{4}[A-Z]{2}$', zip_code)
+    validate_city = city.capitalize() in cities
+    return validate_zipcode and validate_city
+
+def validate_email(email):
+    return re.match(r'^\S+@\S+\.\S+$', email) is not None
+
+def validate_phone(phone):
+    return re.match(r'^\d{8}$', phone) is not None
