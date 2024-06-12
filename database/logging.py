dfaeeb1c09d2a5309585ebe1a38def7c0dc5b77b
MellowZippy1 <basicconsideration1101@gmail.com>
2024-06-12 12:02:57 +0200

    see_logs() now prints decrypted logs, log_activity adds an encrypted log

--- database/logging.py
+++ database/logging.py
@@ -1,8 +1,13 @@
 from datetime import datetime
 from database.database import send_query
+from database.auth import decrypt_data, encrypt_data
+
 
 def see_logs():
-    print("TODO")
+    logs = send_query('''SELECT * FROM logs''')
+    for row in logs:
+        print(', '.join([decrypt_data(cell) for cell in row]))
+
 
 def log_activity(username, description, additional_info=None, suspicious=0):
     date = datetime.now().strftime("%Y-%m-%d")
@@ -10,5 +15,6 @@ def log_activity(username, description, additional_info=None, suspicious=0):
 
     send_query(f'''
     INSERT INTO logs (date, time, username, description, additional_info, suspicious)
-    VALUES ({date}, {time}, {username}, {description}, {additional_info}, {suspicious})
+    VALUES ({encrypt_data(date)}, {encrypt_data(time)}, {encrypt_data(username)}, 
+    {encrypt_data(description)}, {encrypt_data(additional_info)}, {encrypt_data(suspicious)})
     ''')
