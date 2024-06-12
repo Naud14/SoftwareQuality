3b2054f40341eab744e8fda91ee9528adf62a46a
Naud14 <n.drost14@gmail.com>
2024-06-12 10:22:18 +0200

    Add validate file

--- auth.py
+++ auth.py
@@ -1,10 +1,7 @@
 import random
 from datetime import datetime
-import re
 # Define roles
 roles = ["super_admin", "system_admin", "consultant"]
-cities = ["Rotterdam", "Oldenzaal", "Budapest", "Kathmandu", "Moskou", "Lelystad", "Stockholm", "Ruinerwold", "Liverpool", "Penemunde"]
-
 
 # Add a user function
 def add_user(conn, username, password, role, first_name, last_name):
@@ -60,26 +57,3 @@ def create_unique_id():
     checksum = sum(first_nine) % 10
 
     return ''.join(map(str, first_nine)) + str(checksum)
-
-def check_first_name(name):
-    return name.isalpha()
-
-def validate_(age):
-    return age.isdigit() and 0 < int(age) <= 125
-
-def validate_gender(gender):
-    return gender.lower() in ['male', 'female']
-
-def validate_weight(weight):
-    return weight.replace('.', '', 1).isdigit() and 0 < int(weight) <= 700
-
-def validate_address(street, house_number, zip_code, city):
-    validate_zipcode = re.match(r'^\d{4}[A-Z]{2}$', zip_code)
-    validate_city = city.capitalize() in cities
-    return validate_zipcode and validate_city
-
-def validate_email(email):
-    return re.match(r'^\S+@\S+\.\S+$', email) is not None
-
-def validate_phone(phone):
-    return re.match(r'^\d{8}$', phone) is not None
